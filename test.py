69d7507dc5530fd137c33dbe9cca56e3e917af3c
e7gx <abalgabo@gmail.com>
2024-06-14 22:45:17 +0300

    .

---
+++ test.py
@@ -0,0 +1,31 @@
+# try :
+#     if True:
+#         print("Hello")
+# except:
+#     print("Something went wrong")
+    
+    
+    
+#     abdullah = 99
+
+    
+student_grade:int = 65
+    
+if student_grade >= 95:
+        print("abdullah is the best")
+elif student_grade >= 90:  
+        print("A")      
+elif student_grade >= 85:
+        print("B+")
+elif student_grade >= 80:
+        print("B")
+elif student_grade >= 75:
+        print("C+")
+elif student_grade >= 70:
+        print("C")
+elif student_grade >= 65:
+        print("D+")
+elif student_grade >= 60:
+        print("D")
+else:
+        print("F")
